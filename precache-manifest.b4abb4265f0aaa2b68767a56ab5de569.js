d0465b58f8384e5e4b361706862d3eb57f6ad960
taroalan <taroalan@outlook.com>
2020-05-17 11:43:55 +0800

    update site

---
+++ precache-manifest.b4abb4265f0aaa2b68767a56ab5de569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7b57e6607d5bd43ad4855a8bc21d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e036f75b87a49f1ef604",
+    "url": "/static/css/main.da8ffcee.chunk.css"
+  },
+  {
+    "revision": "dbb40bc6ca2f4eb09153",
+    "url": "/static/js/2.defa8c91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.defa8c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e036f75b87a49f1ef604",
+    "url": "/static/js/main.e2d691c9.chunk.js"
+  },
+  {
+    "revision": "77b7fea5572c627fed44",
+    "url": "/static/js/runtime-main.bd2298fb.js"
+  },
+  {
+    "revision": "313552995a61818183cd549a19474f83",
+    "url": "/static/media/logo.31355299.jpg"
+  }
+]);
